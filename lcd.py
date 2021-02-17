5deac446d69e7393be986cb3631ebdd728c5f069
Delta <davidjoshuasim@gmail.com>
2021-02-17 01:09:23 -0800

    test

--- lcd.py
+++ lcd.py
@@ -4,7 +4,7 @@ from RPLCD.gpio import CharLCD
 from RPi import GPIO
 GPIO.setwarnings(False)
 
-lcd = CharLCD(pin_rs=27, pin_rw=18, pin_e=29, cols=16, rows=2, pins_data=[37,35,33,31], numbering_mode=GPIO.BOARD)
+lcd = CharLCD(pin_rs=27, pin_e=29, cols=16, rows=2, pins_data=[37,35,33,31], numbering_mode=GPIO.BOARD)
 
 lcd.clear()
 lcd.write_string(sys.argv[1])

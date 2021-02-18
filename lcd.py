187ae6868fb03c80f089fc2c12a765d16bc37c35
Delta <davidjoshuasim@gmail.com>
2021-02-17 23:24:05 -0800

    runs

--- lcd.py
+++ lcd.py
@@ -14,7 +14,7 @@ GPIO.setup(19,GPIO.OUT)
 GPIO.setup(26,GPIO.OUT)
 
 # lcd = CharLCD(numbering_mode=GPIO.BOARD, pin_rs=27, pin_rw=25, pin_e=29, pins_data=[37, 35, 33, 31], cols=16, rows=2) # pin_rw is grounded
-lcd = CharLCD(pin_rs=0, pin_e=5, pins_data=[26, 19, 13, 6], cols=16, rows=2)
+lcd = CharLCD(numbering_mode=GPIO.BCM, pin_rs=0, pin_e=5, pins_data=[26, 19, 13, 6], cols=16, rows=2)
 
 lcd.clear()
 lcd.write_string(sys.argv[1])

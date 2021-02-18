3f16442a24981781c79734a3d7d30d6600f99550
Delta <davidjoshuasim@gmail.com>
2021-02-17 23:17:46 -0800

    push

--- lcd.py
+++ lcd.py
@@ -3,11 +3,9 @@ import sys
 from RPLCD.gpio import CharLCD
 from RPi import GPIO
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
 
-lcd = CharLCD(pin_rs=27, pin_rw=25, pin_e=29, pins_data=[37, 35, 33, 31], 
-            cols=16, rows=2, 
-            numbering_mode=GPIO.BOARD) # pin_rw is grounded
+# lcd = CharLCD(numbering_mode=GPIO.BOARD, pin_rs=27, pin_rw=25, pin_e=29, pins_data=[37, 35, 33, 31], cols=16, rows=2) # pin_rw is grounded
+lcd = CharLCD(numbering_mode=GPIO.BCM, pin_rs=0, pin_e=5, pins_data=[26, 19, 13, 6], cols=16, rows=2)
 
 lcd.clear()
 lcd.write_string(sys.argv[1])
